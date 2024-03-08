b7d7bcb86b2d1799a71126d83066e842f71dd115
mika <noreply@mikabytes.com>
2024-03-08 15:46:10 +0100

    fixx logout

--- srv/endpoints/logout.js
+++ srv/endpoints/logout.js
@@ -2,11 +2,11 @@ import config from "../config.js"
 
 export default (req, res) => {
   const options = {}
-  if (config.cookieOverrides.path) {
+  if (config.cookieOverrides?.path) {
     config.options.path = config.cookieOverrides.path
   }
-  if (config.cookieOverrides.domain) {
-    options.domain = cookieOverrides.domain
+  if (config.cookieOverrides?.domain) {
+    options.domain = config.cookieOverrides.domain
   }
   res.clearCookie("authToken", options)
   res.sendStatus(200)
